328fe65f7dcfc6edbc2afe26aa8e94ca6f4673ef
Seba <Seba@Seba-PC>
2025-01-25 13:16:09 -0300

    Punto 2: Pais, edad, altura, precio, telefono y valor de coseno

---
+++ Practico1/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais;
+		int edad;
+		double altura;
+		double precio;
+		long telefono;
+		double coseno;
+		pais = "Argentina";
+		edad = 22;
+		altura = 101.5;
+		precio = 599.99;
+		telefono = 388328148;
+		coseno = 0.8775825619;
+		System.out.println("Pais: "+pais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura: "+altura);
+		System.out.println("Precio: "+precio);
+		System.out.println("Telefono: "+telefono);
+		System.out.println("Coseno de 0,5: "+coseno);
+	}
+
+}
