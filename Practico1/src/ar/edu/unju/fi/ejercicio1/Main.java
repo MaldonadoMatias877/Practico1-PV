caa9f03e3ab8a3149359d9471dde6305ed38be5c
Seba <Seba@Seba-PC>
2025-01-25 12:57:51 -0300

    Punto 1: promedio de 5 valores

---
+++ Practico1/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e;
+		double promedio;
+		a=5;
+		b=6;
+		c=7;
+		d=8;
+		e=9;
+		promedio=(a+b+c+d+e)/5;
+		System.out.println("Promedio: "+promedio);
+	}
+
+}
